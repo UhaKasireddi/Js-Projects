3e8f9dbee1aea6c8067ea68819986cb7aa7dbb36
UhaKasireddi <uhakasireddi1996@gmail.com>
2023-09-01 15:12:23 +0530

    toggle updated

--- Projects/toggleDarkMode/script.js
+++ Projects/toggleDarkMode/script.js
@@ -1,7 +1,9 @@
 let contentE1 = document.getElementById("content");
 let buttonE1 = document.getElementById("but");
+let headH1 = document.getElementById("head")
 buttonE1.addEventListener("click", ()=>{
 
     contentE1.classList.toggle("togg");
+    headH1.classList.toggle("head2")
 
 })
